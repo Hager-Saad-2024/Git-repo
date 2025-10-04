95f440d91fda1d0f3f577aec2757b370cbb826fd
Hager-Saad-2024 <hagersaad9711@gmail.com>
2025-10-04 17:03:40 +0300

    this is the first change in the script

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a simple script to demonstrate basic shell commands
+
+# Print a hello world message
+echo "Hello, World this is a new change"
+
+# List files in the current directory
+echo "Listing files in the current directory:"
+ls
+
+# Display the current date and time
+echo "Current date and time:"
+date
